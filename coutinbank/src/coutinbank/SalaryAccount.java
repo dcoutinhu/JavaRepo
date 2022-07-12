454407f431e094d98d5b3df6e3891992cad99da6
dcoutinhu <diegoengcontrole@gmail.com>
2022-07-12 14:01:59 -0300

    Refs #5: Creating class

---
+++ coutinbank/src/coutinbank/SalaryAccount.java
@@ -0,0 +1,10 @@
+package coutinbank;
+
+public class SalaryAccount extends Account {
+
+	public SalaryAccount(String agency, int accountNumber) {
+		super(agency, accountNumber);
+		// TODO Auto-generated constructor stub
+	}
+
+}
