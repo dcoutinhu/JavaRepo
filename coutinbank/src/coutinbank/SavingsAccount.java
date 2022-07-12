07c6661076e99dd18b3c6a8dc07f5cfbaa1c0dbd
dcoutinhu <diegoengcontrole@gmail.com>
2022-07-12 14:00:04 -0300

    Refs #3: Updating class

--- coutinbank/src/coutinbank/SavingsAccount.java
+++ coutinbank/src/coutinbank/SavingsAccount.java
@@ -4,8 +4,9 @@ public class SavingsAccount extends Account {
 	
 	public SavingsAccount(String agency, int accountNumber) {
 		super(agency, accountNumber);
-		// TODO Auto-generated constructor stub
 	}
+	
+	
 }
 	
 
